8c16d01469abe04578046bedde477318e39f6b29
James Thomas <jamesjoethomas@gmail.com>
2019-12-02 21:58:46 -0800

    small fix

--- merge_cl_shell.tcl
+++ merge_cl_shell.tcl
@@ -1,6 +1,6 @@
 set name [lindex $argv 0]
 open_checkpoint $env(AWS_FPGA)/hdk/cl/examples/cl_dram_dma/build/checkpoints/fleet_shell.dcp
-read_checkpoint -cell WRAPPER_INST/CL merged_shell_kernel.dcp
+read_checkpoint -cell WRAPPER_INST/CL merged_shell_${name}.dcp
 report_route_status
 report_timing -setup
 report_timing -hold
