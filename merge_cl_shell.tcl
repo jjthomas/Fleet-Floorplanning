9bdc8aa90a378bfb2b535c9230ceb48a754dbf69
James Thomas <jamesjoethomas@gmail.com>
2019-12-02 21:43:35 -0800

    reduced output of merge step

--- merge_cl_shell.tcl
+++ merge_cl_shell.tcl
@@ -2,6 +2,7 @@ set name [lindex $argv 0]
 open_checkpoint $env(AWS_FPGA)/hdk/cl/examples/cl_dram_dma/build/checkpoints/fleet_shell.dcp
 read_checkpoint -cell WRAPPER_INST/CL merged_shell_kernel.dcp
 report_route_status
-report_timing_summary
+report_timing -setup
+report_timing -hold
 write_checkpoint -force ${name}_combined.dcp
 
